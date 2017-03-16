737bdb7c7d9569903be539f5e7089339de9a8d10
Mikhail Popov <m.popov@crystals.ru>
2017-03-17 02:26:12 +0300

    Update Introduction.java

--- Lab02/src/main/java/ru/spbstu/telematics/popov/lab02/Introduction.java
+++ Lab02/src/main/java/ru/spbstu/telematics/popov/lab02/Introduction.java
@@ -2,30 +2,6 @@ package ru.spbstu.telematics.popov.lab02;
 
 public class Introduction {
     public static void main(String[] args) {
-	System.out.println("Hello!");
+	System.err("Hello!");
     }
-	
-	void HhHhHhH() {
-	for (;;) {  // Noncompliant; end condition omitted
-		  // ...
-	}
-	int k=10;
-	for(int i=0; i<10; i++){
-		k++;
-	}
-	System.out.println(k);
-	System.out.println("Hlksdf");
-	}
-	
-	
-	void test_divide() {
-  int z = 0;
-  if (unknown()) {
-    // ..
-    z = 3;
-  } else {
-    // ..
-  }
-  z = 1 / z; // Noncompliant, possible division by zero
-}
 }
