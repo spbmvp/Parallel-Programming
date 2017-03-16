83d8223e94accf2ec8201a3a1cb14fe198af8b96
Mikhail Popov <m.popov@crystals.ru>
2017-03-17 02:49:41 +0300

    Update Introduction.java

--- Lab02/src/main/java/ru/spbstu/telematics/popov/lab02/Introduction.java
+++ Lab02/src/main/java/ru/spbstu/telematics/popov/lab02/Introduction.java
@@ -3,9 +3,5 @@ package ru.spbstu.telematics.popov.lab02;
 public class Introduction {
     public static void main(String[] args) {
 	System.err("Hello!");
-	    ////lknm,kjhbjjknml;,kjnbjnmk,
-	    while(true){
-		;
-	    }
     }
 }
