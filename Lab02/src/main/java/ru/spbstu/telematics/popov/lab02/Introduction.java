bfddb4c4df0b8cbe7bea7461e828818b16df4754
Mikhail Popov <m.popov@crystals.ru>
2017-03-17 01:45:35 +0300

    deleted error

--- Lab02/src/main/java/ru/spbstu/telematics/popov/lab02/Introduction.java
+++ Lab02/src/main/java/ru/spbstu/telematics/popov/lab02/Introduction.java
@@ -2,12 +2,6 @@ package ru.spbstu.telematics.popov.lab02;
 
 public class Introduction {
     public static void main(String[] args) {
-        for(;;) {
-	    ;
-        }
-	while(true){
-		;
-	}
-	System.exit(0);
-    }
+	    //DoNothing;
+	    System.out.println("Hello!");
 }
