e8081cb2659cdb69c4df31618b1f1d9744616d34
Mikhail Popov <m.popov@crystals.ru>
2017-04-28 02:19:47 +0300

    Refactoring and Init Lab2 and Lab3

---
+++ src/main/java/ru/spbstu/telematics/popov/lab02/Lab2.java
@@ -0,0 +1,9 @@
+package ru.spbstu.telematics.popov.lab02;
+
+public class Lab2 {
+    public static void main(String[] args) {
+        MyMap<String, String> myMap = new MyMap<>();
+
+        String s = myMap.put("1", "a");
+    }
+}
