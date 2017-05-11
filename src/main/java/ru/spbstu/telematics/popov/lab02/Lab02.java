782c82b8c031911b74fd800f10e69e208d72f0ed
Mikhail Popov <m.popov@crystals.ru>
2017-05-11 16:06:10 +0300

    Lab2 part1

--- src/main/java/ru/spbstu/telematics/popov/lab02/Lab02.java
+++ src/main/java/ru/spbstu/telematics/popov/lab02/Lab02.java
@@ -4,6 +4,15 @@ public class Lab02 {
     public static void main(String[] args) {
         MyMap<String, String> myMap = new MyMap<>();
 
-        String s = myMap.put("1", "a");
+        myMap.put("1", "a");
+        myMap.put("2", "b");
+        myMap.put("3", "a");
+        myMap.put("4", "d");
+
+        myMap.remove("1");
+        myMap.remove("2");
+        myMap.put("1", "a");
+        myMap.put("2", "b");
+        myMap.remove("2","b");
     }
 }
--- src/main/java/ru/spbstu/telematics/popov/lab02/MyMap.java
+++ src/main/java/ru/spbstu/telematics/popov/lab02/MyMap.java
@@ -1,42 +1,139 @@
 package ru.spbstu.telematics.popov.lab02;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class MyMap<K,V> implements Map<K,V>{
+/**
+ * Подобие HashMap, наследуется от intarface Map
+ *
+ * @param <K> - ключ, не должен повторяться
+ * @param <V> - значение
+ * @see Map
+ */
+public class MyMap<K, V> implements Map<K, V> {
+    List<K> keys;
+    List<V> values;
+    List<Integer> links;
+
+    public MyMap() {
+        this.keys = new LinkedList<>();
+        this.values = new LinkedList<>();
+        this.links = new LinkedList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.keys.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.links.size() == 0;
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return this.keys.contains(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return this.values.contains(value);
     }
 
     @Override
     public V get(Object key) {
-        return null;
+        return values.get(getValueIndex(key));
+    }
+
+    private int getValueIndex(Object key) {
+        int i = keys.indexOf(key);
+        for (int l = 0; l < links.size(); l += 2) {
+            if (links.get(l) == i) {
+                return links.get(l + 1);
+            }
+        }
+        return -1;
+    }
+
+    private int getLinkIndex(Object key) {
+        int i = keys.indexOf(key);
+        for (int l = 0; l < links.size(); l += 2) {
+            if (links.get(l) == i) {
+                return l;
+            }
+        }
+        return -1;
     }
 
     @Override
     public V put(K key, V value) {
-        return null;
+        int i = keys.indexOf(key);
+        int j = values.indexOf(value);
+        int l = getLinkIndex(key);
+        if (l != -1 && values.get(links.get(l + 1)) == value) {
+            return value;
+        }
+        if (i == -1 && j == -1) {
+            keys.add(key);
+            values.add(value);
+            links.add(keys.size() - 1);
+            links.add(values.size() - 1);
+        } else if (i == -1) {
+            keys.add(key);
+            links.add(keys.size() - 1);
+            links.add(j);
+        } else if (j == -1) {
+            int index = links.get(l + 1);
+            if (!isAnotherLinkValue(values.get(index), key)) {
+                values.remove(index);
+                rebaseLinks(index);
+            }
+            values.add(value);
+            links.add(l + 1, values.size() - 1);
+            links.remove(l + 2);
+        } else {
+            links.add(l + 1, j);
+            links.remove(l + 1);
+        }
+        return value;
+    }
+
+    private void rebaseLinks(Integer indexRebase) {
+        for (int l = 1; l < links.size(); l += 2) {
+            if (links.get(l) > indexRebase) {
+                links.add(l, links.get(l) - 1);
+                links.remove(l + 1);
+            }
+        }
+    }
+
+    private boolean isAnotherLinkValue(Object value, Object key) {
+        int i = values.indexOf(value);
+        int j = keys.indexOf(key);
+        for (int l = 0; l < links.size(); l += 2) {
+            if (links.get(l + 1) == i && links.get(l) != j) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public V remove(Object key) {
+        int l = getLinkIndex(key);
+        if (l != -1) {
+            V v = values.get(getValueIndex(key));
+            int i = links.get(l + 1);
+            if (!isAnotherLinkValue(values.get(i), key)) {
+                values.remove(i);
+                rebaseLinks(i);
+            }
+            i = links.get(l);
+            keys.remove(i);
+            links.remove(l + 1);
+            links.remove(l);
+            return v;
+        }
         return null;
     }
 
@@ -47,21 +144,47 @@ public class MyMap<K,V> implements Map<K,V>{
 
     @Override
     public void clear() {
-
+        values.clear();
+        keys.clear();
+        links.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        Set<K> set = new HashSet<>();
+        for (K key: keys ) {
+            set.add(key);
+        }
+        return set;
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return values;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        Set<Entry<K, V>> s = new HashSet<>();
+        for (int k = 0; k < links.size(); k+=2) {
+            final int i = k;
+            s.add(new Entry<K, V>() {
+                @Override
+                public K getKey() {
+                    return keys.get(links.get(i));
+                }
+
+                @Override
+                public V getValue() {
+                    return values.get(links.get(i+1));
+                }
+
+                @Override
+                public V setValue(V value) {
+                    return null;
+                }
+            });
+        }
+        return s;
     }
 }
