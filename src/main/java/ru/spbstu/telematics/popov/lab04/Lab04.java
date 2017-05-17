a9fc1cd26ea82cdb0ed0ecf32364838ad9a19f28
Mikhail Popov <m.popov@crystals.ru>
2017-05-18 02:20:15 +0300

    Lab04 part1

--- src/main/java/ru/spbstu/telematics/popov/lab04/Lab04.java
+++ src/main/java/ru/spbstu/telematics/popov/lab04/Lab04.java
@@ -1,85 +1,73 @@
 package ru.spbstu.telematics.popov.lab04;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Random;
 
 public class Lab04 {
     public static void main(String[] args) throws InterruptedException, IOException {
         Integer id = new Random().nextInt(1000);
-        Thread resiver = runResiver(id);
         int mcPort = 12345;
         String mcIPStr = "230.1.1.1";
-        DatagramSocket udpSocket = null;
-        Message mes = new Message(id, "Hello");
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(mes);
+        runReceiver(mcIPStr, mcPort);
+        Message msg = new Message(id, "Hello");
         try {
-            udpSocket = new DatagramSocket();
-            InetAddress mcIPAddress = InetAddress.getByName(mcIPStr);
-            byte[] msg = baos.toByteArray();
-            DatagramPacket packet = new DatagramPacket(msg, msg.length);
-            packet.setAddress(mcIPAddress);
-            packet.setPort(mcPort);
             for (int i = 0; i < 100; i++) {
-                udpSocket.send(packet);
-                System.out.println("                    Sent a  multicast message: " + mes.getMessege());
-                Thread.sleep(5000);
+                msg.setMessege(" hello " + i);
+                sendMessage(msg, mcPort, mcIPStr);
+                System.out.println("                    Sent a  multicast message: " + msg.getMessege());
+                Thread.sleep(1000);
             }
             System.out.println("Exiting application");
-
-            udpSocket.close();
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static Thread runResiver(int id) {
+    private static void sendMessage(Message msg, int mcPort, String mcIPStr) throws IOException {
+        InetAddress mcIPAddress = InetAddress.getByName(mcIPStr);
+        DatagramSocket udpSocket = new DatagramSocket();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(msg);
+        byte[] msgByteArray = baos.toByteArray();
+        DatagramPacket packet = new DatagramPacket(msgByteArray, msgByteArray.length);
+        packet.setAddress(mcIPAddress);
+        packet.setPort(mcPort);
+        udpSocket.send(packet);
+        udpSocket.close();
+    }
+
+    private static Thread runReceiver(String mcIPStr, int mcPort) {
         Thread t = new Thread(() -> {
-            int mcPort = 12345;
-            String mcIPStr = "230.1.1.1";
             MulticastSocket mcSocket = null;
             InetAddress mcIPAddress = null;
             try {
                 mcIPAddress = InetAddress.getByName(mcIPStr);
                 mcSocket = new MulticastSocket(mcPort);
-                System.out.println("Multicast Receiver running at:"
-                        + mcSocket.getLocalSocketAddress());
                 mcSocket.joinGroup(mcIPAddress);
-
-                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-                for (int i = 0; i < 1000000; i++) {
+                DatagramPacket packet = new DatagramPacket(new byte[8096], 8096);
+                while (!Thread.currentThread().isInterrupted()) {
                     mcSocket.receive(packet);
                     ByteArrayInputStream bais = new ByteArrayInputStream(packet.getData());
                     ObjectInputStream ois = new ObjectInputStream(bais);
                     Message msg = (Message) ois.readObject();
-                    System.out.println("[Multicast  Receiver] Received: " + msg.getMessege() + " от " + msg.getIdClient());
+                    handlingMessage(msg);
                 }
                 mcSocket.leaveGroup(mcIPAddress);
                 mcSocket.close();
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
+            } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         });
         t.start();
         return t;
+    }
+
+    private static void handlingMessage(Message msg) {
+        System.out.println("[Multicast  Receiver] Received: " + msg.getMessege() + " от " + msg.getIdClient());
     }
 }
